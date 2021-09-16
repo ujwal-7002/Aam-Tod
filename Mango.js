01137641d436880f29cf96c2d612fe27ee43df05
ujwal-7002 <78682746+ujwal-7002@users.noreply.github.com>
2021-09-16 18:46:48 +0530

    Add files via upload

--- Mango.js
+++ Mango.js
@@ -1,23 +1,29 @@
 class mango{
-	constructor(x,y){
+	constructor(x,y,r){
 		var options={
 			isStatic: true,
 			restitution:0,
 			friction:1,
 		}
-	this.body=Bodies.circle(x,y,50,options);
-	this.width=50;
-	this.height=50;
-	this.image= loadImage("sprites/mango.png");
-	World.add(world,this.body);
-	};
-display(){
-	var pos= this.body.postion;
-	var angle = this.body.angle;
-	push();
-	rotate(angle);
-	imageMode(CENTER)
-	image(this.image, this.x, this.y, this.width, this.height);
-	pop();
-}
+	    this.x=x;
+		this.y=y;
+		this.r=r;
+		this.image=loadImage("sprites/mango.png");
+		this.body=Bodies.circle(this.x, this.y, this.r, options);
+		World.add(world, this.body);
+	}
+
+	display()
+	{
+		var Pos=this.body.position;	
+		push();
+		translate(Pos.x, Pos.y);
+		// rectMode(CENTER);
+		rotate(this.body.angle);
+		fill(255,0,255);
+		imageMode(CENTER);
+		ellipseMode(CENTER);
+		image(this.image, 0,0,this.r*2, this.r*2)
+		pop();
+ }
 }
--- sketch.js
+++ sketch.js
@@ -2,9 +2,11 @@ const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
+const Constraint = Matter.Constraint;
 var tree;
 var stone;
-var mango1 ,mango2, mango3, mango4 , mango5; 
+var mango1 ,mango2, mango3, mango4 , mango5;
+var Cons; 
 function preload()
 {
 	boy=loadImage("sprites/boy.png");
@@ -20,12 +22,12 @@ function setup() {
 	tree = new Tree(600,500);
 	ground = new Ground(width/2, 600, width, 20);
   stone = new Stone(200,200);
-  mango1 = new mango(900,300); 
-  mango2= new mango(1000,300);
-  mango3= new mango(950, 250);
-  mango4= new mango(900, 350);
-  mango5= new mango(950, 350)
-	
+  mango1 = new mango(1000,150,20); 
+  mango2= new mango(1100,180,20);
+  mango3= new mango(1050, 100,20);
+  mango4= new mango(850, 250,20);
+  mango5= new mango(1150, 250,20);
+  Cons= new launcher(stone.body,{x:243,y:420});
 Engine.run(engine);
   
 }
@@ -42,8 +44,30 @@ function draw() {
   mango3.display();
   mango4.display();
   mango5.display();
-  drawSprites();
+  Cons.display();  
+  drawSprites()
+  detectCollision(stone,mango1);
+  detectCollision(stone, mango2);
+  detectCollision(stone, mango3);
+  detectCollision(stone, mango4);
+  detectCollision(stone, mango5);
+  
+}
+function mouseDragged() {
+  Matter.Body.setPosition(stone.body, { x: 200, y: 200});
+
+}
+function mouseReleased() {
+  Cons.fly();
 }
 
+function detectCollision(lstone,lmango){
+  mangoBodyPosition-lmango.body.position
+  stoneBodyPosition=lstone.body.position
 
-
+  var distance=dist(stoneBodyPosition.x, stoneBodyPosition.y, mangoBodyPosition.x, mangoBodyPosition.y)
+    if(distance<-lmango.r+lstone.r)
+    {
+      Matter.Body.setStatic(lmango.body,false);
+    }
+}
